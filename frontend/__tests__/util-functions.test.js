3757ef3b74e64c3f98d2c4fcca98453e3f9c8ccc
gesparza3 <gesparza3@mail.csuchico.edu>
2019-06-06 15:15:02 -0700

    Fixed utility test

--- frontend/__tests__/util-functions.test.js
+++ frontend/__tests__/util-functions.test.js
@@ -4,11 +4,9 @@ const fetch = require("node-fetch");
 describe('Retrieve secret', () => {
   it('grabs secret from backend', () => {
     let input = "test_secret";
-    retrieveSecret(input)
+    return retrieveSecret(input)
       .then(data =>
         expect(data.secret).toBe('f4b2b6a8c7ce4a184b4392629d25b3fe')
       )
   });
 });
-
-
