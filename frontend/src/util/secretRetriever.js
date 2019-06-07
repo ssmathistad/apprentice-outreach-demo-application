1b8a7ec4ab07589bfa56b4d7c381c1ff8f87563c
gesparza3 <gesparza3@mail.csuchico.edu>
2019-06-07 13:22:26 -0700

    Added known host and fixed bug

--- frontend/src/util/secretRetriever.js
+++ frontend/src/util/secretRetriever.js
@@ -4,5 +4,5 @@ export const retrieveSecret = (input) => {
   let headers = {"Content-Type": "application/json"};
   let domain = process.env.REACT_APP_DOMAIN;
   return fetch("http://" + domain + ":8000/?input=" + input, {headers, })
-    .then(res => res.status())
+    .then(res => res.json())
 }
