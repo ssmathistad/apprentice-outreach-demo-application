b354916e43e90d8e71704cc1178e24068a4f56fc
gesparza3 <gesparza3@mail.csuchico.edu>
2019-06-07 13:59:24 -0700

    Fixed fetch test

--- frontend/__tests__/util-functions.test.js
+++ frontend/__tests__/util-functions.test.js
@@ -1,12 +1,18 @@
 import { retrieveSecret } from '../src/util/secretRetriever.js';
 const fetch = require("node-fetch");
 
+// describe('Retrieve secret', () => {
+//   it('grabs secret from backend', () => {
+//     let input = "test_secret";
+//     return retrieveSecret(input)
+//       .then(data =>
+//         expect(data.secret).toBe('f4b2b6a8c7ce4a184b4392629d25b3fe')
+//       )
+//   });
+// });
 describe('Retrieve secret', () => {
   it('grabs secret from backend', () => {
     let input = "test_secret";
-    return retrieveSecret(input)
-      .then(data =>
-        expect(data.secret).toBe('f4b2b6a8c7ce4a184b4392629d25b3fe')
-      )
+    return expect(retrieveSecret(input).then(data => data.secret)).resolves.toBe('f4b2b6a8c7ce4a184b4392629d25b3fe');
   });
 });
